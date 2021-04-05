3af48bd736b9b6975a20d20b685277152ebc873b
Tracy Golden <tracy80s2003@gmail.com>
2021-04-04 22:06:11 -0500

    added Home component and more styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
+    <>
     <Header />
+    <Home />
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Digital Marketer</h1>
+                <a href="#" className="btn-main-offer">Contact Me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Home
