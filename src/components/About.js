2ed19a75a4bf73d32e243458558b0a80babc0879
Tracy Golden <tracy80s2003@gmail.com>
2021-04-06 17:59:12 -0500

    trying to add pdf to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import resumeData from './resumeData.json';
 import headshot from '../Images/headshot.jpeg';
+import { Document } from 'react-pdf'
 
 export default class About extends Component {
   render() {
@@ -11,6 +12,13 @@ export default class About extends Component {
             <div className="ten columns">
               <p className="lead">
               {resumeData.aboutme}
+              </p>
+            </div>
+          </div>
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              <Document file="./public/documents/resume.pdf" />
               </p>
             </div>
           </div>
