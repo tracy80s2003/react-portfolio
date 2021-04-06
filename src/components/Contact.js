c6dd30bd7df8c408f2d92996e5a1077501652098
Tracy Golden <tracy80s2003@gmail.com>
2021-04-06 09:01:52 -0500

    updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,15 +18,23 @@ export default class Contact extends Component {
               <h5><strong>GitHub:</strong> {resumeData.imagebaseurl}</h5>
             </div>
           </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>
-                </h4>
-              </div>
-            </aside>
-          </div>
+          <div>&nbsp;</div>
+          <form id="contact-form" onSubmit={this.handleSubmit} method="POST">
+            <div className="form-group">
+              <label htmlFor="name">Enter Your Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" aria-describedby="emailHelp" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </section>
-        );
+      );
   }
 }
